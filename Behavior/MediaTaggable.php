00c271a4d865262d8b6342b7e50cee8c74cc7ff9
Nicolas BONNIOT <nicolas@devgiants.fr>
2017-05-30 15:58:45 +0200

    Fix bug on tag getter

--- Behavior/MediaTaggable.php
+++ Behavior/MediaTaggable.php
@@ -10,6 +10,7 @@ namespace Lch\MediaBundle\Behavior;
 
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Lch\MediaBundle\Entity\Tag;
 
@@ -22,11 +23,11 @@ trait MediaTaggable
     private $tags;
 
     /**
-     * @return ArrayCollection
+     * @return Collection
      */
-    public function getTags(): ArrayCollection
+    public function getTags(): Collection
     {
-        if(!$this->tags instanceof ArrayCollection) {
+        if(!$this->tags instanceof Collection) {
             $this->tags = new ArrayCollection();
         }
         return $this->tags;
