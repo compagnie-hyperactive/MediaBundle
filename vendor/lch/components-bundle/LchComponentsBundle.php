3c0e9acb563cbd7e01310ef437d6ef6a622fa621
Bilel ZARI <bzari@melisagency.fr>
2020-01-06 14:40:12 +0100

    fix gitignore

--- vendor/lch/components-bundle/LchComponentsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Lch\ComponentsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class LchComponentsBundle extends Bundle
-{
-}
--- vendor/lch/translate-bundle/Model/Interfaces/TranslatableInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Lch\TranslateBundle\Model\Interfaces;
-
-interface TranslatableInterface
-{
-
-}
